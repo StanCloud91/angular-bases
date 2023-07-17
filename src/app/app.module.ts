e76f56e8b0cf3eb9bc788c7214ae50845c469220
Stalin Salgado <superldu91@gmail.com>
2023-07-17 18:33:31 -0500

    Fin de Seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { counterModule } from './counter/component/counter.module';
+import { heroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    counterModule,
+    heroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
